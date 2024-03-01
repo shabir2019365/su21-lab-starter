46700ef6116c31b3cc697213c32aa92486934b6e
Muhammad Shabir Khan <shabbirkhan1701114@gmail.com>
2024-03-01 12:11:18 +0500

    Completed exercise1_Part02: All the test cases have passed

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -22,5 +22,33 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    
+    //initialize counts to zero
+    dna_seq->A_count = 0;
+    dna_seq->C_count = 0;
+    dna_seq->G_count = 0;
+    dna_seq->T_count = 0;
+
+    //To iterate through the each charactor and count them
+    char *current_nucleotide = dna_seq->sequence;
+    while (*current_nucleotide != '\0') {
+    // count nucleotide by using case sensitive coparision
+        if (*current_nucleotide == 'A' || *current_nucleotide == 'a') {
+        	dna_seq->A_count++;
+	}
+	
+        else if (*current_nucleotide == 'C' || *current_nucleotide == 'c') {
+        	dna_seq->C_count++;
+	}
+
+        else if (*current_nucleotide == 'G' || *current_nucleotide == 'g') {
+        	dna_seq->G_count++;
+	}
+
+        else if (*current_nucleotide == 'T' || *current_nucleotide == 't') {
+        	dna_seq->T_count++;
+	}
+	current_nucleotide++;
+    } 
     return;
 }
--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -42,7 +42,17 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.T_count == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    // Additional test case for the lowercase nucleotide 
+    DNA_sequence dna_seq_3;
+    strcpy(dna_seq_3.sequence, "actgtac");
 
+    compute_nucleotide_occurrences(&dna_seq_3);
+    assert(dna_seq_3.A_count == 2);
+    assert(dna_seq_3.C_count == 2);
+    assert(dna_seq_3.G_count == 1);
+    assert(dna_seq_3.T_count == 2);
+
+    printf("Additional test case also passed!\n");
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
     return 0;
