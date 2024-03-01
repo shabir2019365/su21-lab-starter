5bf054486f51e7691c51bd7300f1bede142ca6f2
Muhammad Shabir Khan <shabbirkhan1701114@gmail.com>
2024-03-01 09:51:13 +0500

    completed task: Exercise1-Part 01: Adding num occurance funtion in ex1.c and test this test have passed.

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -1,3 +1,4 @@
+// Muhammad Shabir Khan - Exercise1 - 1st March 2024
 #include <string.h>
 #include "ex1.h"
 
@@ -6,8 +7,14 @@ There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
-    /* TODO: implement num_occurances */
-    return 0;
+	int count = 0;
+	while (*str != '\0') {  // Implement the num occurance by 2nd method using a null terminator to iterate all over the string
+		if (*str == letter){ //string will check if the letter is matched or not
+			count++;
+		}
+		str++;
+	}
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
