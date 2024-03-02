ca16ecf42cada483cf6ffd2df46e1d7293154bb4
Muhammad Shabir Khan <shabbirkhan1701114@gmail.com>
2024-03-02 13:20:00 +0500

    Completed Exercise4: All the test cases have passed.

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,18 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+     /* TODO: Implement ll_has_cycle */
+  node *slow_ptr = head;
+  node *fast_ptr = head;
+
+  while (fast_ptr && fast_ptr->next) {
+    slow_ptr = slow_ptr->next;  // Advance slow_ptr by one node
+    fast_ptr = fast_ptr->next->next;  // Advance fast_ptr by two nodes
+
+    if (slow_ptr == fast_ptr) {
+      return 1;  // Cycle detected
+    }
+  }
+
+  return 0;  // No cycle found
 }
